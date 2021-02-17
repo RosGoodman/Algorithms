f83c8e593aaa8df34c88d8d5311eabcf93e35a20
Ros <pavel.official@bk.ru>
2021-02-17 18:18:21 +0300

    Task_2 done

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Task_2
+{
+    class Program
+    {
+        /// O(1 + N*N*N + 1)
+        /// O(2 + N^3)
+        /// В соответствии с правилом 3 методички сокращаем:
+        /// O(N^3)
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;    //O(1)
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)
+                    {
+                        int y = 0;
+
+                        if (j != 0)
+                        {
+                            y = k / j;
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;
+                    }
+                }
+            }
+            return sum;     //O(1)
+        }
+    }
+}
