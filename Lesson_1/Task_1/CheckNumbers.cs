d9e5646f2c488eb3d1fc5369d66d7ae8d747bb38
Ros Goodman <pavel.official@bk.ru>
2021-02-17 16:17:50 +0300

    add tests, task1 done

---
+++ Lesson_1/Task_1/CheckNumbers.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Task_1
+{
+    public static class CheckNumbers
+    {
+        /// <summary>Проверка, является ли число простым.</summary>
+        /// <param name="n">Проверяемое число.</param>
+        /// <returns>True/False - простое/не простое</returns>
+        public static bool SimpleNumb(int number)
+        {
+            int d = 0;
+            int i = 2;
+
+            if (number < 0) return false;   //простые числа - натуральные больше 0.
+
+            while (i < number)
+            {
+                if (number % i == 0) d++;
+
+                i++;
+            }
+
+            if (d == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- Lesson_1/Task_1/Program.cs
+++ Lesson_1/Task_1/Program.cs
@@ -7,31 +7,10 @@ namespace Task_1
         static void Main(string[] args)
         {
             int n = 11;
-            SimpleNumb(n);
+            CheckNumbers.SimpleNumb(n);
 
-            Console.WriteLine(SimpleNumb(n));
+            Console.WriteLine(CheckNumbers.SimpleNumb(n));
             Console.ReadLine();
-        }
-
-        /// <summary>Проверка, является ли число простым.</summary>
-        /// <param name="n">Проверяемое число.</param>
-        /// <returns>True/False - простое/не простое</returns>
-        private static bool SimpleNumb(int number)
-        {
-            int d = 0;
-            int i = 2;
-
-            while(i < number)
-            {
-                if (number % i == 0) d++;
-
-                i++;
-            }
-
-            if (d == 0)
-                return true;
-            else
-                return false;
         }
     }
 }
