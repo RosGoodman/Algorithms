878a82a115cd1e461b4b69b7da178155ffc15387
Ros Goodman <pavel.official@bk.ru>
2021-02-18 14:13:00 +0300

    Lesson-1 done

--- Lesson_1/Task2_UnitTest/Task3_UnitTest.cs
+++ Lesson_1/Task2_UnitTest/Task3_UnitTest.cs
@@ -7,7 +7,7 @@ namespace Task3_UnitTests
     public class Task3_UnitTest
     {
         [TestMethod]
-        public void TestRecursive()
+        public void Test_FibonachiRecurive()
         {
             // Arrange
             int number1 = 1;
@@ -24,7 +24,29 @@ namespace Task3_UnitTests
             // Assert
             Assert.AreEqual(1, actual1, "Ожидается 1");
             Assert.AreEqual(2, actual2, "Ожидается 2");
-            Assert.AreEqual(8, actual3, "Ожидается 8");
+            Assert.AreEqual(5, actual3, "Ожидается 5");
+            Assert.AreEqual(89, actual4, "Ожидается 89");
+        }
+
+        [TestMethod]
+        public void Test_FibonacciCycle()
+        {
+            // Arrange
+            int number1 = 1;
+            int number2 = 3;
+            int number3 = 5;
+            int number4 = 11;
+
+            // Act
+            int actual1 = Program.FibonacciCycle(number1);
+            int actual2 = Program.FibonacciCycle(number2);
+            int actual3 = Program.FibonacciCycle(number3);
+            int actual4 = Program.FibonacciCycle(number4);
+
+            // Assert
+            Assert.AreEqual(1, actual1, "Ожидается 1");
+            Assert.AreEqual(2, actual2, "Ожидается 2");
+            Assert.AreEqual(5, actual3, "Ожидается 5");
             Assert.AreEqual(89, actual4, "Ожидается 89");
         }
     }
