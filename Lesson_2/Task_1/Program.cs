4a6ac18f3f58f512e670363717f1a54a6ce4e642
Ros Goodman <pavel.official@bk.ru>
2021-02-20 17:56:08 +0300

    nothing

--- Lesson_2/Task_1/Program.cs
+++ Lesson_2/Task_1/Program.cs
@@ -1,12 +1,12 @@
 ﻿
-
 namespace Task_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Nodes nodes = new Nodes();
+            nodes.AddNode(1);
         }
     }
 }
