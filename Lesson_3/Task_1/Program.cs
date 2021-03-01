58293abcb105756e0ed348bab8a656ad6306296e
Ros Goodman <pavel.official@bk.ru>
2021-03-01 16:28:07 +0300

    start work

--- Lesson_3/Task_1/Program.cs
+++ Lesson_3/Task_1/Program.cs
@@ -6,7 +6,27 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 5000;
+            float[,] floatArray = Program.FloatCoordinatesGen(count);
+        }
+
+        /// <summary></summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        private static float[,] FloatCoordinatesGen(int count)
+        {
+            float[,] floatArray = new float[count, 2];
+            Random random = new Random();
+
+
+            for (int i = 0; i < count; i++)
+            {
+                floatArray[i, 0] = random.Next(254);
+                floatArray[i, 0] = (float)(floatArray[i, 0] + random.NextDouble());
+                floatArray[i, 1] = random.Next(254);
+                floatArray[i, 1] = (float)(floatArray[i, 0] + random.NextDouble());
+            }
+            return floatArray;
         }
     }
 }
