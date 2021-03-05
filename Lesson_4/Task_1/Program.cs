b6df0183cd0978dc232c20f776ee390997b0062d
Ros <pavel.official@bk.ru>
2021-03-05 18:05:04 +0300

    add string generator

--- Lesson_4/Task_1/Program.cs
+++ Lesson_4/Task_1/Program.cs
@@ -1,11 +1,16 @@
-﻿
+﻿using System;
+
 namespace Task_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int count = 10000;
+            int length = 5;
+            string[] str = StringGenerator.GetRandomStringArray(count, length);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Lesson_4/Task_1/StringGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Task_1
+{
+    internal static class StringGenerator
+    {
+        /// <summary>Сгенерировать массив строк случайных символов.</summary>
+        /// <param name="count">Количество строк.</param>
+        /// <param name="legth">Количество символов.</param>
+        /// <returns>Массив строк.</returns>
+        internal static string[] GetRandomStringArray(int count, int length)
+        {
+            string[] str = new string[count];
+            Random _random = new Random(Environment.TickCount);
+            string chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+            StringBuilder builder = new StringBuilder(length);
+
+            for (int i = 0; i < count; i++)
+            {
+                builder.Clear();
+                for (int j = 0; j < length; ++j)
+                    builder.Append(chars[_random.Next(chars.Length)]);
+
+                str[i] = builder.ToString();
+            }
+
+            return str;
+        }
+    }
+}
