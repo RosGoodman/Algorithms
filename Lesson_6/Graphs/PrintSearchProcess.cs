8c98d89ebb2b3ad74f1e73ed9229d1d6fe76977c
Ros <pavel.official@bk.ru>
2021-03-12 16:04:59 +0300

    add PrintSearchProcess class

---
+++ Lesson_6/Graphs/PrintSearchProcess.cs
@@ -0,0 +1,31 @@
+﻿
+using Graphs.Graph;
+using System;
+
+namespace Graphs
+{
+    public class PrintSearchProcess
+    {
+        public void ProcessBFS(Node[] nodes)
+        {
+            string wave = string.Empty;
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                wave += nodes[i].Value + " ";
+            }
+            Console.WriteLine(wave);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="searchType"></param>
+        public void FoundNode(int value, string searchType)
+        {
+            if(value == -1)
+                Console.WriteLine();
+            Console.WriteLine("{0}: {1}", searchType, value);
+        }
+    }
+}
