4a0b6375ce2ccf698ce55213c556b5bea34329df
Ros Goodman <pavel.official@bk.ru>
2021-03-17 11:44:07 +0300

    add TestsClass

---
+++ Lesson_6/Graph_Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Graph_Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
