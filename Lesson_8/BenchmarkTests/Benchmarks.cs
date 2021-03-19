1b1e1758e92832aa37228c9bcde6f6b0f24328d3
Ros <pavel.official@bk.ru>
2021-03-19 17:24:30 +0300

    add benchmark proj

---
+++ Lesson_8/BenchmarkTests/Benchmarks.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using Task_1;
+
+namespace BenchmarkTests
+{
+    public class Benchmark
+    {
+        #region TestQuickSort
+
+        [Benchmark]
+        [ArgumentsSource(nameof(GetArray))]
+        public void TestDistanceForPointClass(int[] array, int x)
+        {
+            Sortings.Quicksort(array, 0, x);
+        }
+
+        public IEnumerable<object[]> GetArray()
+        {
+            int[] array = RandomArray.GetRandomArray(10000, 0, 9999);
+            yield return new object[] { array, 5000};
+        }
+
+        #endregion
+    }
+}
